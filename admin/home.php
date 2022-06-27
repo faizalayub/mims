128aaee3639c3cbd3f0fc445f1f082c89e157e4b
Faizal <faizalayub29@gmail.com>
2022-06-28 07:27:37 +0800

    Export table

--- admin/home.php
+++ admin/home.php
@@ -4,24 +4,22 @@
    <head>
       <?php include "inc/head.php"; ?>
       <style>
-        .user-form {
-        display: inline-block !important;
-        width: 100% !important;
-        margin: 2px 0 !important;
-        padding: 5px 0 !important;
-    }
+            .user-form {
+               display: inline-block !important;
+               width: 100% !important;
+               margin: 2px 0 !important;
+               padding: 5px 0 !important;
+            }
 
-    .user-submit{
-      margin-top: 20px;
-      float: right;
-    }
+            .user-submit{
+               margin-top: 20px;
+               float: right;
+            }
 
-    .card-body{
-      width: 100% !important;
+            .card-body{
+               width: 100% !important;
 
-    }
-
-
+            }
       </style>
     
 
@@ -53,55 +51,55 @@
             </div>
             <!-- /.content-header -->
             <!-- Main content -->
-            <section class="content">
-               <div class="container-fluid">
-                  <!-- Small boxes (Stat box) -->
-                  <div class="row">
-                  </div>
-                  <!-- /.row -->
-                  <!-- Main row -->
-                  <div class="row">
-                     <!-- Left col -->
-                     <section class="col-lg-12 connectedSortable">
-                        <div class="card">
-                           <!-- /.card-header -->
-                           <div class="card-body">
-                              <table id="example" class="table table-bordered table-striped"  style="text-align: center;font-size:12px">
-                                 <thead>
-                                    <tr>
-                                       <th>Category</th>
-                                       <th>Kedah</th>
-                                       <th>Sabah</th>
-                                       <th>Johor</th>
-                                       <th>Terengganu</th>
-                                       <th>Labuan</th>
-                                       <th>Perak</th>
-                                       <th>Kelantan</th>
-                                       <th>Pulau Pinang</th>
-                                       <th>Melaka</th>
-                                       <th>Pahang</th>
-                                       <th>Miri</th>
-                                       <th>Kuching</th>
-                                    </tr>
-                                 </thead>
+               <section class="content">
+                  <div class="container-fluid">
+                     <!-- Small boxes (Stat box) -->
+                     <div class="row">
+                     </div>
+                     <!-- /.row -->
+                     <!-- Main row -->
+                     <div class="row">
+                        <!-- Left col -->
+                        <section class="col-lg-12 connectedSortable">
+                           <div class="card">
+                              <!-- /.card-header -->
+                              <div class="card-body">
+                                 <table id="example" class="table table-bordered table-striped"  style="text-align: center;font-size:12px">
+                                    <thead>
+                                       <tr>
+                                          <th>Category</th>
+                                          <th>Kedah</th>
+                                          <th>Sabah</th>
+                                          <th>Johor</th>
+                                          <th>Terengganu</th>
+                                          <th>Labuan</th>
+                                          <th>Perak</th>
+                                          <th>Kelantan</th>
+                                          <th>Pulau Pinang</th>
+                                          <th>Melaka</th>
+                                          <th>Pahang</th>
+                                          <th>Miri</th>
+                                          <th>Kuching</th>
+                                       </tr>
+                                    </thead>
+                                    
+                                    <tbody id="tableData">
                                  
-                                 <tbody id="tableData">
-                                
-                                 </tbody>
-                                
-                              </table>
+                                    </tbody>
+                                 
+                                 </table>
+                              </div>
+                              <!-- /.card-body -->
                            </div>
-                           <!-- /.card-body -->
-                        </div>
-                     </section>
-                     <!-- right col -->
+                        </section>
+                        <!-- right col -->
+                     </div>
+                     <!-- /.row (main row) -->
                   </div>
-                  <!-- /.row (main row) -->
-               </div>
-               <!-- /.container-fluid -->
-            </section>
-            <!-- /.content -->
-         </div>
+                  <!-- /.container-fluid -->
+               </section>
+               <!-- /.content -->
+            </div>
          <!-- /.content-wrapper -->
          <!-- <footer class="main-footer">
             <?php include "inc/footer.php"; ?>
@@ -116,31 +114,41 @@
       <?php include "inc/js.php"; ?>
       <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
       <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
-         <script>
-        function getCategoryListOnLoad(){
-      
-        $.ajax({
-          type:'post',
-          url:'action.php',
-          data:{"action":"getCategoryListOnLoad"},
-          success:function(result){
-            var result_obj = JSON.parse(result);
-      
-            if(result_obj.valid){
-              document.getElementById("tableData").innerHTML = result_obj.html;
-             $('#example').DataTable({
-                "pageLength": 50,
-                "lengthChange": false,
-                "bPaginate": false
-             });
-      
-            }else{
-      
-            }
-          }
-        })
-      }
 
-    </script>  
+      <script src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"></script>
+      <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
+      <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js"></script>
+      <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js"></script>
+
+      <script>  
+         function getCategoryListOnLoad(){
+            $.ajax({
+               type:'post',
+               url:'action.php',
+               data:{"action":"getCategoryListOnLoad"},
+               success:function(result){
+                  var result_obj = JSON.parse(result);
+            
+                  if(result_obj.valid){
+                     document.getElementById("tableData").innerHTML = result_obj.html;
+
+                     $('#example').DataTable({
+                        "pageLength": 50,
+                        "lengthChange": false,
+                        "bPaginate": false,
+                        "dom": 'ZBflrtip',
+                        "buttons": [
+                           'csv', 'excel', 'print'//'copy', 'csv', 'excel', 'pdf', 'print'
+                        ]
+                     });
+                  }
+               }
+            })
+         }
+      </script>  
+
    </body>
 </html>
